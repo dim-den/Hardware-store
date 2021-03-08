bc547c0a41c9a847d52eb45375102a0d151a1eaa
Dmitriy <dimden999@gmail.com>
2021-03-08 16:02:16 +0300

    Added some resources, triggers, changed button templates. Created new custom control (NumericUpDown). Realized undo, redo functionality

---
+++ OOPLab6/Commands/UndoCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLab6.Commands
+{
+    public class UndoCommand : IUndoCommands
+    {
+        private readonly Func<object, Device> execute;
+        private readonly Action<object> unexecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public UndoCommand(Func<object, Device> execute, Action<object> unexecute)
+        {
+            this.execute = execute;
+            this.unexecute = unexecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+           var res = this.execute(parameter);
+            SearchUserControl.undoCommandManager.Add(this, res);
+        }
+
+        public void Unexecute(object parameter)
+        {
+            this.unexecute(parameter);
+        }
+    }
+}
